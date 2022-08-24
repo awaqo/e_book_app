f4965422e622a38413f4d8237f4ea94f1378dc57
awaqo <aqiljawadalfurqon@gmail.com>
2022-08-24 21:34:03 +0700

    add color and text style, google font dependencies

---
+++ lib/themes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// COLOR
+Color whiteColor = Color(0xFFFFFFFF);
+Color slate700Color = Color(0xFF334155);
+Color slate500Color = Color(0xFF64748B);
+Color slate400Color = Color(0xFF94A3B8);
+Color slate100Color = Color(0xFFF1F5F9);
+Color greenColor = Color(0xFF098B5C);
+
+// TEXT
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+
+TextStyle semiBold20 = GoogleFonts.poppins(color: slate700Color, fontSize: 20, fontWeight: semibold);
+TextStyle semiBold16 = GoogleFonts.poppins(color: slate700Color, fontSize: 16, fontWeight: semibold);
+TextStyle semiBold14 = GoogleFonts.poppins(color: slate700Color, fontSize: 14, fontWeight: semibold);
+TextStyle semiBold12 = GoogleFonts.poppins(color: slate500Color, fontSize: 12, fontWeight: semibold);
+
+TextStyle regular14 = GoogleFonts.poppins(color: slate400Color, fontSize: 14, fontWeight: regular);
+TextStyle regular12 = GoogleFonts.poppins(color: slate500Color, fontSize: 12, fontWeight: regular);
+
+TextStyle medium14 = GoogleFonts.poppins(color: slate500Color, fontSize: 14, fontWeight: medium);
+TextStyle medium12 = GoogleFonts.poppins(color: slate400Color, fontSize: 12, fontWeight: medium);
+TextStyle medium10 = GoogleFonts.poppins(color: slate500Color, fontSize: 10, fontWeight: medium);
